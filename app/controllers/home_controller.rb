c77dfa2d3987592705791a4db272d565348e8a69
nilpatel <nil.p@simformsolutions.com>
2023-02-28 14:42:38 +0530

    feat: TE7-T115 introduction to rails
    create a home controller and display 'hello #{name}' from simform'
    also add college,hobbies,strength,weakness,etc.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+    def index
+        @name="Nil Patel"
+        @college="LD Colleg of Engineering"
+        @university="Gujarat Technological University"
+        @branch="IT"
+        @strength="Adaptability"
+        @weekness="Regularity"
+        @hobbie="Exploring Life"
+    end
+end
