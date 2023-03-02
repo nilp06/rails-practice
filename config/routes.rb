80be22b46133290aa0a714ec5150b43bb431abd7
nilpatel <nil.p@simformsolutions.com>
2023-03-02 18:28:47 +0530

    feat: TE7-T115 introduction to rails add products controller and create crud operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # get 'products/index'
+  # get 'products/:id', to: 'products#show'
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
 end
