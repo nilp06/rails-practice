0bbb60586d5d9f7bd3d41742b6bd307fab7f2a9a
nilpatel <nil.p@simformsolutions.com>
2023-03-02 18:42:06 +0530

    feat: TE7-T115 introduction to rails add validation and callback

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+  validates :name ,uniqueness: true,presence: true
+  validates :price, presence: true
+  validates :name_should_start_with_D, on:create
+  #custom validation
+  private
+  def name_should_start_with_D
+    false
+  end
+
+  #callback
+  before_save :change_category
+  def chage_category
+    self.categoty="all"
+  end
 end
