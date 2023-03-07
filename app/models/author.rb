85c3ff478f0aae61663f9f4380e530a843869b1d
nilpatel <nil.p@simformsolutions.com>
2023-03-07 10:17:41 +0530

    feat:TE7-T117 active record migrations:add validations to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+  validates :first_name,presence: true,length: {minimum:3, maximum: 20}
+  validates :last_name,presence: true,length: {minimum:3, maximum: 20}
+  validates_date :dob,presence: true,on_or_before: lambda{Date.current} ,after: '1950-01-01'
+  validates :email,presence: true,format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ },uniqueness: true
 end
