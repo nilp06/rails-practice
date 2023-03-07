1fdfb0f3fe0679e832a1cdb1f8ca985a855bae65
nilpatel <nil.p@simformsolutions.com>
2023-03-07 10:30:55 +0530

    feat:TE7-T117 active record migrations:remove column age from author model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_044954) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_045958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_044954) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "age"
   end
 
 end
