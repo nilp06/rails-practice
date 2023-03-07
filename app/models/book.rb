e87f9f5caa59f6be25e4f4b6a49f27c989040e47
nilpatel <nil.p@simformsolutions.com>
2023-03-07 11:14:05 +0530

    feat:TE7-T117 active record migrations:add model book

---
+++ app/models/book.rb
@@ -0,0 +1,4 @@
+class Book < ApplicationRecord
+  validates :name,presence: true
+  validates :price,numericality: { greater_than_or_equal_to: 0, less_than: BigDecimal(10**4) },format: { with: /\A\d{1,4}(\.\d{1,2})?\z/ }
+end
