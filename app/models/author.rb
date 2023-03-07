d02d2f6e9dc3382b0ad8cd50356a3e84e135c52e
nilpatel <nil.p@simformsolutions.com>
2023-03-07 12:13:12 +0530

    feat:TE7-T117 active record migrations:add reference author to book model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ApplicationRecord
+  has_many  :books,dependent: :destroy
   validates :first_name,presence: true,length: {minimum:3, maximum: 20}
   validates :last_name,presence: true,length: {minimum:3, maximum: 20}
   validates_date :dob,presence: true,on_or_before: lambda{Date.current} ,after: '1950-01-01'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_053514) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_054707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_053514) do
     t.decimal "price", precision: 6, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "books", "authors"
 end
