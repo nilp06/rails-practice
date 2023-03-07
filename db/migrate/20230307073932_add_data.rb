f9a883bf21fc72d6b911de18bd3fa54930994904
nilpatel <nil.p@simformsolutions.com>
2023-03-07 13:38:10 +0530

    feat:TE7-T117 active record migrations:add data with migration for the author and book

---
+++ db/migrate/20230307073932_add_data.rb
@@ -0,0 +1,10 @@
+class AddData < ActiveRecord::Migration[7.0]
+  def change
+    5.times do |i|
+      Author.create(first_name:"auth#{i}",last_name:"david",dob:"07-07-200#{i}",email:"authp#{i}@gmail.com")
+    end
+    5.times do |i|
+      Book.create(name:"Book#{i}",price:i*100,author_id:1)
+    end
+  end
+end
