43fd9fd87bc6b75d5147ca8f9d344ad24457e03b
nilpatel <nil.p@simformsolutions.com>
2023-04-12 13:55:04 +0530

    feat:TE7-T120 active-record-query-interface: create employee crud

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,50 @@
+class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      redirect_to @employee
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+
+    redirect_to employees_path, status: :see_other
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :age, :no_of_order, :full_time_available, :salary)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :employees
   resources :users, only: %i[new create]
   resources :sessions, only: %i[new create]
   get 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
