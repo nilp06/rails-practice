b8299eaafd8c041e3748aed3f48edd483a10f966
nilpatel <nil.p@simformsolutions.com>
2023-04-17 14:28:48 +0530

    feat:TE7-T126 working with js in rails:add authentication functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :users, only: %i[new create]
+  resources :sessions, only: %i[new create]
+  get 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
+  get 'home/index'
   root 'home#index'
 end
