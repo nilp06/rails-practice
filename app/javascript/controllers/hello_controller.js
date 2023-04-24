dc4e01f9c955f8e5855d8d4ab5aa7ac14dbfb65f
nilpatel <nil.p@simformsolutions.com>
2023-04-24 15:16:02 +0530

    add js functionality for practice

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!";
+    // alert("This is alert");
+  }
+  greet() {
+    console.log("Hello, Stimulus!", this.element);
+    alert("This is greet action.");
   }
 }
