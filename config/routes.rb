b9783d40d7eb6cc06d186d8b63f18901716822ee
nilpatel <nil.p@simformsolutions.com>
2023-04-26 17:37:24 +0530

    feat:TE7-T127 Layouts And Rendering In Rails:add order crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products do
+    resources :orders
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
