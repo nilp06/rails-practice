d88786f05000aa9edec47c4530ebf7e2e6cabdf3
nilpatel <nil.p@simformsolutions.com>
2023-04-27 10:52:52 +0530

    feat:TE7-T127 Layouts And Rendering In Rails:add restriction to order index page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :check_admin?
   def index
     @orders = Order.includes(:product).all
+  end
+
+  private
+
+  def check_admin?
+    return unless is_admin?
+
+    redirect_back fallback_location: root_path, flash: { danger: 'You have not Acess to this page.' }
   end
 end
