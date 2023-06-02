8f02133b2dbe2d5320d26d5d8d05d12548782f11
nilpatel <nil.p@simformsolutions.com>
2023-06-02 18:37:02 +0530

    refactor: TE7-T118 active record validations: refator some files as per purva mam guidelines

--- app/helpers/faculties_helper.rb
+++ app/helpers/faculties_helper.rb
@@ -1,4 +1,2 @@
 module FacultiesHelper
-  DepartmentChoices=[["Select department","0"],["IT","IT"],["CE","CE"]]
-  DesignationChoices=[["Select Designation","0"],["HOD","HOD"],["Sr. Prof","Sr. Prof"],["Ass. Prof","Ass. Prof"],["Prof","Prof"]]
 end
